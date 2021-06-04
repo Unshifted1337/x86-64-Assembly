6ee028b1f7d820dc4796e6e225e1a8b03b5e3de3
Unshiftx86 <73394656+Unshifted1337@users.noreply.github.com>
2021-06-04 08:59:45 -0400

    Create registers.asm

---
+++ AssemblyBasics/registers.asm
@@ -0,0 +1,5 @@
+mov rax, 0xdeadbeef ; move the number 0xdeadbeef into rax
+mov eax, 42         ; move the number 42 into eax, the lower 32 bits of rax
+mov ax, 0xff        ; move the number 0xff into ax, the lowest 16 bits of rax
+mov ah, 0xf         ; move the number 0xf into ah, the high 8 bits of ax
+mov al, 0xa         ; move the number 0xa into al, the low 8 bits of ax
